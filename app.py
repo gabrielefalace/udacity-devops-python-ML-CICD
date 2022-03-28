34e4bf97035edc0ea518177be179bb59c1f9fd31
Gabriele Falace <gabrielefalace@gmail.com>
2022-03-28 14:38:22 +0200

    small change to trigger CI/CD

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = "<h3>Sklearn Prediction Home - Newest version</h3>"
+    html = "<h3>Sklearn Prediction Home - with small changes</h3>"
     return html.format(format)
 
 # TO DO:  Log out the prediction value
